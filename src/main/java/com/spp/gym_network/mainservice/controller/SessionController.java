cbfdce26d3b6171cc7d3a50efd4a354a1baa72f8
Michael Fediuchenko <mixei112299@gmail.com>
2021-01-30 05:15:53 +0200

    Create 3 different types of auth services to choose the best fitted one

--- src/main/java/com/spp/gym_network/mainservice/controller/SessionController.java
+++ src/main/java/com/spp/gym_network/mainservice/controller/SessionController.java
@@ -1,12 +1,40 @@
 package com.spp.gym_network.mainservice.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+import static java.util.Map.entry;
 
 @RestController
 public class SessionController {
-    @RequestMapping("/")
-    public String helloAdmin() {
-        return "hello admin";
+    @GetMapping("/user")
+    @PreAuthorize("hasRole('USER')")
+    Map<String, String> user(Authentication authentication) {
+        return Map.ofEntries(
+                entry("endpoint", "USER"),
+                entry("actor", authentication.getName())
+        );
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    Map<String, String> admin(Authentication authentication) {
+        return Map.ofEntries(
+                entry("endpoint", "ADMIN"),
+                entry("actor", authentication.getName())
+        );
+    }
+
+    @GetMapping("/system")
+    @PreAuthorize("hasRole('SYSTEM')")
+    Map<String, String> system(Authentication authentication) {
+        return Map.ofEntries(
+                entry("endpoint", "SYSTEM"),
+                entry("actor", authentication.getName())
+        );
     }
 }
---
+++ src/main/java/com/spp/gym_network/mainservice/service/auth/Role.java
@@ -0,0 +1,7 @@
+package com.spp.gym_network.mainservice.service.auth;
+
+public enum Role {
+    USER,
+    ADMIN,
+    SYSTEM,
+}
