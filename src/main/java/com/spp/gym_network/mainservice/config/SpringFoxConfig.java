77773496dd744c289ec53c9bfb9d7cb1300e9a27
Michael Fediuchenko <mixei112299@gmail.com>
2021-01-30 05:14:38 +0200

    Add swagger config, Update app.props and deps

---
+++ src/main/java/com/spp/gym_network/mainservice/config/SpringFoxConfig.java
@@ -0,0 +1,68 @@
+package com.spp.gym_network.mainservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.*;
+
+import java.util.Collections;
+
+@Profile("!prod")
+@Configuration
+public class SpringFoxConfig {
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "My REST API",
+                "The system is designed to keep track of the evaluation of the network of gyms, convenient customer planning\n" +
+                        "          classes, to obtain statistics about customers, coaches, gyms and classes. Also AIS\n" +
+                        "          you can register and consume in gyms.",
+                "API TOS",
+                "Terms of service",
+                new Contact("Michael Fediuchenko", "www.github.com/MikLay", "mixei112299@gmail.com"),
+                "License of API",
+                "API license URL",
+                Collections.emptyList());
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    /**
+     * SwaggerUI information
+     */
+
+    @Bean
+    UiConfiguration uiConfig() {
+        return UiConfigurationBuilder.builder()
+                .deepLinking(true)
+                .displayOperationId(false)
+                .defaultModelsExpandDepth(1)
+                .defaultModelExpandDepth(1)
+                .defaultModelRendering(ModelRendering.EXAMPLE)
+                .displayRequestDuration(false)
+                .docExpansion(DocExpansion.NONE)
+                .filter(false)
+                .maxDisplayedTags(null)
+                .operationsSorter(OperationsSorter.ALPHA)
+                .showExtensions(false)
+                .tagsSorter(TagsSorter.ALPHA)
+                .supportedSubmitMethods(UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS)
+                .validatorUrl(null)
+                .build();
+    }
+
+}
