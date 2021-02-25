22a69b46c6f56fb4195cc0588999a4817ed30fe1
Michael Fediuchenko <mixei112299@gmail.com>
2021-02-26 00:54:24 +0200

    Finally create migrations for app init, Update dependencies

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,194 +1,49 @@
--- we don't know how to generate root <with-no-name> (class Root) :(
--- create table users
--- (
---     id         bigint auto_increment
---         primary key,
---     email      varchar(255) null,
---     first_name varchar(255) null,
---     last_name  varchar(255) null,
---     password   varchar(255) null,
---     constraint UK_6dotkott2kjsp8vw4d0m25fb7
---         unique (email)
--- );
+create table roles
+(
+    role_id int auto_increment
+        primary key,
+    name    varchar(255) null
+);
+
+INSERT INTO gym_network.roles (role_id, name) VALUES (1, 'ROLE_USER');
+INSERT INTO gym_network.roles (role_id, name) VALUES (2, 'ROLE_ADMIN');
 
 
+create table users
+(
+    id bigint auto_increment
+        primary key,
+    account_verified bit not null,
+    email varchar(255) null,
+    first_name varchar(255) null,
+    last_name varchar(255) null,
+    password varchar(255) null,
+    token varchar(255) null,
+    constraint UK_6dotkott2kjsp8vw4d0m25fb7
+        unique (email)
+);
 
+create table user_roles
+(
+    id bigint not null,
+    role_id int not null,
+    primary key (id, role_id),
+    constraint FKh8ciramu9cc9q3qcqiv4ue8a6
+        foreign key (role_id) references roles (role_id),
+    constraint FKnmt12ry8d6a20i1un4cj253k6
+        foreign key (id) references users (id)
+);
 
--- create table roles
--- (
---     role_id int          not null
---         primary key,
---     role    varchar(255) null
--- );
---
--- REPLACE INTO `roles` VALUES (1,'ADMIN');
---
--- create table users
--- (
---     user_id  bigint       not null
---         primary key,
---     active   bit          null,
---     email    varchar(255) null,
---     password varchar(255) null
--- );
---
--- create table user_role
--- (
---     user_id bigint not null,
---     role_id int    not null,
---     primary key (user_id, role_id),
---     constraint FKj345gk1bovqvfame88rcx7yyx
---         foreign key (user_id) references users (user_id),
---     constraint FKt7e7djp752sqn6w22i6ocqy6q
---         foreign key (role_id) references roles (role_id)
--- );
-
--- create table gyms
--- (
--- 	gym_id int auto_increment
--- 		primary key,
--- 	country varchar(45) not null,
--- 	town varchar(45) not null,
--- 	area varchar(45) null,
--- 	street varchar(45) not null,
--- 	building int not null,
--- 	office int null,
--- 	fine int not null,
--- 	post_index varchar(45) not null,
--- 	email varchar(45) not null,
--- 	description varchar(2500) not null
--- );
---
--- create table equipment
--- (
--- 	equipment_id int auto_increment
--- 		primary key,
--- 	name varchar(45) not null,
--- 	type varchar(45) not null,
--- 	`condition` varchar(45) not null,
--- 	photo_url varchar(250) not null,
--- 	gym_id int not null,
--- 	constraint photo_gym_id
--- 		foreign key (gym_id) references gyms (gym_id)
--- 			on update cascade on delete cascade
--- );
---
--- create index gym_id_idx
--- 	on equipment (gym_id);
---
--- create table gym_photos
--- (
--- 	photo_id int auto_increment
--- 		primary key,
--- 	photo_url varchar(250) not null,
--- 	gym_id int not null,
--- 	constraint gym_photos_gyms_gym_id_fk
--- 		foreign key (gym_id) references gyms (gym_id)
--- 			on update cascade on delete cascade
--- );
-
--- create table clients
--- (
--- 	client_id int auto_increment
--- 		primary key,
--- 	lastname varchar(45) not null,
--- 	firstname varchar(45) not null,
--- 	middlename varchar(45) null,
--- 	photo_url varchar(750) not null,
--- 	phone varchar(45) not null,
--- 	birth_date date not null,
--- 	constraint clients_users_user_id_fk
--- 		foreign key (client_id) references users (user_id)
--- 			on update cascade on delete cascade
--- );
---
--- create table coaches
--- (
--- 	coach_id int auto_increment
--- 		primary key,
--- 	lastname varchar(45) not null,
--- 	firstname varchar(45) not null,
--- 	middlename varchar(45) null,
--- 	email varchar(45) not null,
--- 	phone varchar(45) not null,
--- 	country varchar(45) not null,
--- 	town varchar(45) not null,
--- 	area varchar(45) null,
--- 	street varchar(45) not null,
--- 	building varchar(45) not null,
--- 	flat int null,
--- 	sport_rang varchar(45) null,
--- 	payment int not null,
--- 	sex tinyint(1) not null,
--- 	photo_url varchar(250) not null,
--- 	constraint coaches_users_user_id_fk
--- 		foreign key (coach_id) references users (user_id)
--- 			on update cascade
--- );
---
--- create table subscriptions
--- (
--- 	subscription_id int auto_increment
--- 		primary key,
--- 	start_date date not null,
--- 	end_date date not null,
--- 	workout_start_time time not null,
--- 	client_id int null,
--- 	workout_end_time time not null,
--- 	price int not null,
--- 	constraint owner_id
--- 		foreign key (client_id) references clients (client_id)
--- );
---
--- create index client_id_idx
--- 	on subscriptions (client_id);
---
--- create table timetables
--- (
--- 	timetable_id int auto_increment
--- 		primary key,
--- 	day int not null,
--- 	start_time time not null,
--- 	end_time time not null,
--- 	coach_id int not null,
--- 	gym_id int not null,
--- 	constraint coach
--- 		foreign key (coach_id) references coaches (coach_id)
--- 			on delete cascade,
--- 	constraint gym
--- 		foreign key (gym_id) references gyms (gym_id)
--- 			on update cascade on delete cascade
--- );
---
--- create index coach_idx
--- 	on timetables (coach_id);
---
--- create index gym_idx
--- 	on timetables (gym_id);
---
--- create table workouts
--- (
--- 	workout_id int auto_increment
--- 		primary key,
--- 	start_date datetime not null,
--- 	end_time time not null,
--- 	coach_id int null,
--- 	client_id int not null,
--- 	gym_id int not null,
--- 	surcharge int not null,
--- 	constraint client_id
--- 		foreign key (client_id) references clients (client_id),
--- 	constraint coach_id
--- 		foreign key (coach_id) references coaches (coach_id),
--- 	constraint gym_id
--- 		foreign key (gym_id) references gyms (gym_id)
--- );
---
--- create index client_id_idx
--- 	on workouts (client_id);
---
--- create index coach_id_idx
--- 	on workouts (coach_id);
---
--- create index gym_id_idx
--- 	on workouts (gym_id);
+create table secure_tokens
+(
+    id bigint auto_increment
+        primary key,
+    expire_at datetime(6) not null,
+    time_stamp datetime(6) null,
+    token varchar(255) null,
+    user_id bigint null,
+    constraint UK_dlvilc17p76edkguw5kd16xl5
+        unique (token),
+    constraint FKbmjsiygarm7ug34j7icgorpxl
+        foreign key (user_id) references users (id)
+);
