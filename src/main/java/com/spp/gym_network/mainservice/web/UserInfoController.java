d45fcf645b55361ebc00026df957cc1468a20bd0
Michael Fediuchenko <mixei112299@gmail.com>
2021-02-26 01:00:17 +0200

    Create signin, signup, verify controllers

--- src/main/java/com/spp/gym_network/mainservice/web/UserInfoController.java
+++ src/main/java/com/spp/gym_network/mainservice/web/UserInfoController.java
@@ -1,4 +1,4 @@
-package com.spp.gym_network.mainservice.controller;
+package com.spp.gym_network.mainservice.web;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
