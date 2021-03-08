9b9e8fa9610f11c100bfe0cb19fef134e0b95e0b
Michael Fediuchenko <mixei112299@gmail.com>
2021-03-08 03:07:04 +0200

    Remove unused properties

--- src/main/java/com/spp/gym_network/mainservice/MainServiceApplication.java
+++ src/main/java/com/spp/gym_network/mainservice/MainServiceApplication.java
@@ -1,33 +1,14 @@
 package com.spp.gym_network.mainservice;
 
-import com.spp.gym_network.mainservice.util.GymServiceProperties;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.Environment;
-
-import java.io.PrintStream;
 
 @SpringBootApplication
 public class MainServiceApplication {
 
     public static void main(final String[] args) {
         SpringApplication app = new SpringApplication(MainServiceApplication.class);
-        app.setBanner(new Banner() {
-            @Override
-            public void printBanner(final Environment environment, final Class<?>
-                    sourceClass, final PrintStream out) {
-                out.print("\n\n\tGym Network!\n\n".toUpperCase());
-            }
-        });
+        app.setBanner((environment, sourceClass, out) -> out.print("\n\n\tGym Network!\n\n".toUpperCase()));
         app.run(args);
-    }
-
-    final GymServiceProperties gymServiceProperties;
-
-    @Autowired
-    public MainServiceApplication(GymServiceProperties gymServiceProperties) {
-        this.gymServiceProperties = gymServiceProperties;
     }
 }
