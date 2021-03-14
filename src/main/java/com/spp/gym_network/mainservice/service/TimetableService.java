68ad8f1d2a950e3067cc98cc157343804ef13b46
Michael Fediuchenko <mixei112299@gmail.com>
2021-03-14 20:04:38 +0200

    Create Endpoint for creating new Timetables by coach

--- src/main/java/com/spp/gym_network/mainservice/service/TimetableService.java
+++ src/main/java/com/spp/gym_network/mainservice/service/TimetableService.java
@@ -1,12 +1,13 @@
 package com.spp.gym_network.mainservice.service;
 
-import com.spp.gym_network.mainservice.dto.TimetableDTO;
 import com.spp.gym_network.mainservice.model.coach.TimetableEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.sql.Timestamp;
+
 public interface TimetableService {
     Page<TimetableEntity> findMyTimetables(Long id, Pageable page);
 
-    TimetableEntity createTimetable(Long id, TimetableDTO timetable);
+    TimetableEntity createTimetable(Long coachId, Long gymId, Timestamp startTime, Timestamp endTime);
 }
--- src/main/java/com/spp/gym_network/mainservice/service/impl/DefaultTimetableService.java
+++ src/main/java/com/spp/gym_network/mainservice/service/impl/DefaultTimetableService.java
@@ -1,27 +1,55 @@
 package com.spp.gym_network.mainservice.service.impl;
 
-import com.spp.gym_network.mainservice.dto.TimetableDTO;
+import com.spp.gym_network.mainservice.exception.BadRequestException;
+import com.spp.gym_network.mainservice.model.coach.CoachEntity;
 import com.spp.gym_network.mainservice.model.coach.TimetableEntity;
+import com.spp.gym_network.mainservice.model.gym.GymEntity;
+import com.spp.gym_network.mainservice.repository.CoachRepository;
+import com.spp.gym_network.mainservice.repository.GymRepository;
 import com.spp.gym_network.mainservice.repository.TimetableRepository;
 import com.spp.gym_network.mainservice.service.TimetableService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
+
+@Slf4j
 @Service
 public class DefaultTimetableService implements TimetableService {
 
     @Autowired
-    TimetableRepository repository;
+    TimetableRepository timetableRepository;
+
+    @Autowired
+    CoachRepository coachRepository;
+
+    @Autowired
+    GymRepository gymRepository;
 
     @Override
     public Page<TimetableEntity> findMyTimetables(Long id, Pageable page) {
-        return repository.findAllByCoach_Id(id, page);
+        return timetableRepository.findAllByCoach_Id(id, page);
     }
 
     @Override
-    public TimetableEntity createTimetable(Long id, TimetableDTO timetable) {
-        return null;
+    public TimetableEntity createTimetable(Long coachId, Long gymId, Timestamp startTime, Timestamp endTime) {
+        GymEntity gym = gymRepository.getOne(gymId);
+        CoachEntity coach = coachRepository.getOne(coachId);
+        TimetableEntity timetable = new TimetableEntity();
+        timetable.setGym(gym);
+        timetable.setCoach(coach);
+        timetable.setStartTime(startTime);
+        timetable.setEndTime(endTime);
+        try {
+            return timetableRepository.save(timetable);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new BadRequestException(e.getMessage());
+        }
     }
+
+
 }
