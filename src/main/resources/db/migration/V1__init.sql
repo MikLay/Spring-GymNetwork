e3be11df982047ff2c5a88a8bdad4c73d84ae09b
Michael Fediuchenko <mixei112299@gmail.com>
2021-03-16 00:35:31 +0200

    Update migrations, Add test data migration

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,46 +1,40 @@
-create table roles
-(
-    role_id int auto_increment
-        primary key,
-    name    varchar(255) null
-);
-
-INSERT INTO gym_network.roles (role_id, name) VALUES (1, 'ROLE_USER');
-INSERT INTO gym_network.roles (role_id, name) VALUES (2, 'ROLE_ADMIN');
-INSERT INTO gym_network.roles (role_id, name) VALUES (3, 'ROLE_COACH');
-INSERT INTO gym_network.roles (role_id, name) VALUES (4, 'ROLE_MANAGER');
-INSERT INTO gym_network.roles (role_id, name) VALUES (5, 'ROLE_CLIENT');
-
-
-create table gyms
+create table if not exists gyms
 (
     id bigint auto_increment
-        primary key,
+    primary key,
     city varchar(255) null,
     country varchar(255) null,
     flat varchar(255) null,
     house varchar(255) null,
     street varchar(255) null,
     email varchar(255) null,
+    fine bigint null,
     phone varchar(255) null
-);
+    );
 
-create table equipment
+create table if not exists equipment
 (
     id bigint auto_increment
-        primary key,
+    primary key,
     equipment_condition varchar(255) null,
     name varchar(255) null,
     type varchar(255) null,
     gym_id bigint null,
     constraint FK8gr8y0dgofxp7vsrqjgp9a3jl
-        foreign key (gym_id) references gyms (id)
-);
+    foreign key (gym_id) references gyms (id)
+    );
 
-create table users
+create table if not exists roles
+(
+    role_id int auto_increment
+    primary key,
+    name varchar(255) null
+    );
+
+create table if not exists users
 (
     id bigint auto_increment
-        primary key,
+    primary key,
     account_verified bit not null,
     birth_date datetime(6) null,
     email varchar(255) null,
@@ -53,18 +47,18 @@ create table users
     sex varchar(255) null,
     token varchar(255) null,
     constraint UK_6dotkott2kjsp8vw4d0m25fb7
-        unique (email)
-);
+    unique (email)
+    );
 
-create table clients
+create table if not exists clients
 (
     id bigint not null
-        primary key,
+    primary key,
     constraint FK1hgwdp9vl25xl9i7s354sifey
-        foreign key (id) references users (id)
-);
+    foreign key (id) references users (id)
+    );
 
-create table coaches
+create table if not exists coaches
 (
     city varchar(255) null,
     country varchar(255) null,
@@ -74,97 +68,99 @@ create table coaches
     payment bigint null,
     rang varchar(255) null,
     id bigint not null
-        primary key,
+    primary key,
     constraint FKbyei1g9vs5d057vur8psubw3x
-        foreign key (id) references users (id)
-);
+    foreign key (id) references users (id)
+    );
 
-create table manager
+create table if not exists manager
 (
     id bigint not null
-        primary key,
+    primary key,
     constraint FKmqwhyh7lyvaoxegkx6nwj5u43
-        foreign key (id) references users (id)
-);
+    foreign key (id) references users (id)
+    );
 
-create table manager_gym
+create table if not exists manager_gym
 (
     manager_id bigint not null,
     gym_id bigint not null,
     constraint FK6ikiyfiw7usa6wx4uu1shum3p
-        foreign key (gym_id) references gyms (id),
+    foreign key (gym_id) references gyms (id),
     constraint FKr7e0mcr0h1y2t8iby6kum84es
-        foreign key (manager_id) references manager (id)
-);
+    foreign key (manager_id) references manager (id)
+    );
 
-create table secure_tokens
+create table if not exists secure_tokens
 (
     id bigint auto_increment
-        primary key,
+    primary key,
     expire_at datetime(6) not null,
     time_stamp datetime(6) null,
     token varchar(255) null,
     user_id bigint null,
     constraint UK_dlvilc17p76edkguw5kd16xl5
-        unique (token),
+    unique (token),
     constraint FKjee0y5bc9uy34jy86boygrjhl
-        foreign key (user_id) references users (id)
-);
+    foreign key (user_id) references users (id)
+    );
 
-create table subscriptions
+create table if not exists subscriptions
 (
     id bigint auto_increment
         primary key,
-    end_day date null,
-    end_time time null,
+    beginning_time time null,
+    ending_time time null,
+    expiring_datetime datetime(6) null,
     price bigint null,
-    start_day date null,
-    start_time time null,
+    starting_datetime datetime(6) null,
     client_id bigint null,
     constraint FKevklhfw4ajv4e8j1cl3c8mceh
         foreign key (client_id) references clients (id)
 );
 
-create table timetable
+
+
+create table if not exists timetable
 (
     id bigint auto_increment
-        primary key,
+    primary key,
     end_time datetime(6) null,
     start_time datetime(6) null,
     coach_id bigint null,
     gym_id bigint null,
     constraint FKc5uuhjii5xeiac1sp3dbe02jm
-        foreign key (gym_id) references gyms (id),
+    foreign key (gym_id) references gyms (id),
     constraint FKqnjr12job944jt2r3to5xfmdp
-        foreign key (coach_id) references coaches (id)
-);
+    foreign key (coach_id) references coaches (id)
+    );
 
-create table user_roles
+create table if not exists user_roles
 (
     id bigint not null,
     role_id int not null,
     primary key (id, role_id),
     constraint FKh8ciramu9cc9q3qcqiv4ue8a6
-        foreign key (role_id) references roles (role_id),
+    foreign key (role_id) references roles (role_id),
     constraint FKnmt12ry8d6a20i1un4cj253k6
-        foreign key (id) references users (id)
-);
+    foreign key (id) references users (id)
+    );
 
-create table workouts
+create table if not exists workouts
 (
     id bigint auto_increment
-        primary key,
-    date date null,
-    end_time time null,
-    start_time time null,
-    surcharge int not null,
+    primary key,
+    end_time datetime(6) null,
+    start_time datetime(6) null,
+    surcharge bigint null,
     client_id bigint null,
     coach_id bigint null,
     gym_id bigint null,
     constraint FK5k4qtecoodyigr0fxew9y4tmg
-        foreign key (coach_id) references coaches (id),
+    foreign key (coach_id) references coaches (id),
     constraint FKi2r9v4var1p9ggmtq4idfnip8
-        foreign key (gym_id) references gyms (id),
+    foreign key (gym_id) references gyms (id),
     constraint FKjo0p8gr9gk1u4wir5mpmdk0t3
-        foreign key (client_id) references clients (id)
-);
+    foreign key (client_id) references clients (id)
+    );
+
