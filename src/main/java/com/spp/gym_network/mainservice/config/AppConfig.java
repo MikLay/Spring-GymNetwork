da7eee6bb1c8a225a658d2626d7a946a09712b44
74R45 <mail2taras@gmail.com>
2021-04-06 16:59:06 +0300

    Add Allowed Methods and Headers to CORS config

--- src/main/java/com/spp/gym_network/mainservice/config/AppConfig.java
+++ src/main/java/com/spp/gym_network/mainservice/config/AppConfig.java
@@ -27,6 +27,9 @@ public class AppConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "OPTIONS")
+                .allowedHeaders("Origin", "Content-Type", "Authorization")
+                .allowedOrigins("http://localhost:3000");
     }
 }
