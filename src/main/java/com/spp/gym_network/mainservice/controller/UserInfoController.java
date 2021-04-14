da080d6be045045b55e176ecee3f5182693bd2b4
Michael Fediuchenko <mixei112299@gmail.com>
2021-04-14 12:22:12 +0300

    Add roles to UserInfoController

--- src/main/java/com/spp/gym_network/mainservice/controller/UserInfoController.java
+++ src/main/java/com/spp/gym_network/mainservice/controller/UserInfoController.java
@@ -2,16 +2,20 @@ package com.spp.gym_network.mainservice.controller;
 
 import com.fasterxml.jackson.annotation.JsonView;
 import com.spp.gym_network.mainservice.dto.JsonViews;
-import com.spp.gym_network.mainservice.dto.UserDTO;
 import com.spp.gym_network.mainservice.dto.mappers.UserMapper;
 import com.spp.gym_network.mainservice.security.CustomUserDetails;
 import com.spp.gym_network.mainservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController()
 public class UserInfoController {
@@ -25,7 +29,14 @@ public class UserInfoController {
 
     @JsonView(JsonViews.Detailed.class)
     @GetMapping("/me")
-    public ResponseEntity<UserDTO> currentUser(@AuthenticationPrincipal CustomUserDetails userDetails) {
-        return ResponseEntity.ok(userMapper.toDto(userService.getUserInformation(userDetails.getId())));
+    public ResponseEntity currentUser(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        Map<Object, Object> model = new HashMap<>();
+        model.put("client", userMapper.toDto(userService.getUserInformation(userDetails.getId())));
+        model.put("roles", userDetails.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList())
+        );
+        return ResponseEntity.ok(model);
     }
 }
